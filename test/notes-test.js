f285001750314578b7ca4277841434f36dc487c2
Courtney Adams <cadams8238@gmail.com>
2018-06-06 13:11:10 -0700

    added basic get tests

--- test/notes-test.js
+++ test/notes-test.js
@@ -34,62 +34,94 @@ describe('Running note tests', function() {
 		return mongoose.disconnect();
 	});
 
-	describe('POST /api/notes', function() {
-		it('should create and return a new note if input is valid', function() {
-			const newNote = {
-				"title": "Cats Rule the World",
-				"content": "The Title Says it All"
-			};
-
-			let res;
-			return chai.request(app)
-				.post('/api/notes')
-				.send(newNote)
-				.then(function(response) {
-					res = response;
-					expect(res).to.have.status(201);
-					expect(res).to.have.header('location');
-					expect(res).to.be.json;
-					expect(res.body).to.be.a('object');
-					expect(res.body).to.have.keys('id', 'title', 'content', 'createdAt', 'updatedAt');
-					expect(res.body.id).to.not.be.null;
-					return Note.findById(res.body.id);
-				})
-				.then(newNote => {
-					expect(res.body.id).to.equal(newNote.id);
-					expect(res.body.title).to.equal(newNote.title);
-					expect(res.body.content).to.equal(newNote.content);
-					expect(new Date(res.body.createdAt)).to.eql(newNote.createdAt);
-					expect(new Date(res.body.updatedAt)).to.eql(newNote.updatedAt);
-				});
-		});
-	});
 
 
-	describe('GET /api/notes/:id', function() {
-		it('should return correct note with matching id', function() {
-			let note;
-			return Note.findOne()
-				.then(randomNote => {
-					note = randomNote;
-					return chai.request(app)
-						.get(`/api/notes/${note.id}`);
-				})
-				.then(res => {
+
+	describe.only('GET TESTS', function() {
+
+		describe('GET /api/note', function() {
+			it('should return all notes', function() {
+				return Promise.all([
+					Note.find(),
+					chai.request(app).get('/api/notes')
+					])
+				.then(([notes, res]) => {
 					expect(res).to.have.status(200);
 					expect(res).to.be.json;
-					expect(res).to.be.a('object');
-					expect(res.body).to.have.keys('id', 'title', 'content', 'createdAt', 'updatedAt');
-					expect(res.body.id).to.not.be.null;
-
-					expect(res.body.id).to.equal(note.id);
-					expect(res.body.title).to.equal(note.title);
-					expect(res.body.content).to.equal(note.content);
-					expect(new Date(res.body.createdAt)).to.eql(note.createdAt);
-					expect(new Date(res.body.updatedAt)).to.eql(note.updatedAt);
+					expect(res.body).to.be.a('array');
+					expect(res.body).to.have.length(notes.length);
+					expect(res.body).to.not.be.null;
 				});
+			});
+		});
+
+		describe('GET /api/notes/:id', function() {
+			it('should return correct note with matching id', function() {
+				let note;
+				return Note.findOne()
+					.then(randomNote => {
+						note = randomNote;
+						return chai.request(app)
+							.get(`/api/notes/${note.id}`);
+					})
+					.then(res => {
+						expect(res).to.have.status(200);
+						expect(res).to.be.json;
+						expect(res).to.be.a('object');
+						expect(res.body).to.have.keys('id', 'title', 'content', 'createdAt', 'updatedAt');
+						expect(res.body.id).to.not.be.null;
+
+						expect(res.body.id).to.equal(note.id);
+						expect(res.body.title).to.equal(note.title);
+						expect(res.body.content).to.equal(note.content);
+						expect(new Date(res.body.createdAt)).to.eql(note.createdAt);
+						expect(new Date(res.body.updatedAt)).to.eql(note.updatedAt);
+					});
+			});
 		});
 	});
+
+
+	describe('POST TESTS', function() {
+
+		describe('POST /api/notes', function() {
+			it('should create and return a new note if input is valid', function() {
+				const newNote = {
+					"title": "Cats Rule the World",
+					"content": "The Title Says it All"
+				};
+
+				let res;
+				return chai.request(app)
+					.post('/api/notes')
+					.send(newNote)
+					.then(function(response) {
+						res = response;
+						expect(res).to.have.status(201);
+						expect(res).to.have.header('location');
+						expect(res).to.be.json;
+						expect(res.body).to.be.a('object');
+						expect(res.body).to.have.keys('id', 'title', 'content', 'createdAt', 'updatedAt');
+						expect(res.body.id).to.not.be.null;
+						return Note.findById(res.body.id);
+					})
+					.then(newNote => {
+						expect(res.body.id).to.equal(newNote.id);
+						expect(res.body.title).to.equal(newNote.title);
+						expect(res.body.content).to.equal(newNote.content);
+						expect(new Date(res.body.createdAt)).to.eql(newNote.createdAt);
+						expect(new Date(res.body.updatedAt)).to.eql(newNote.updatedAt);
+					});
+			});
+		});
+
+	});
+
+
+
+	
+
+
 
 
 
