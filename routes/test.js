7d77fe2d6964463f2efe361ae15e4593e1a4c59e
Courtney Adams <cadams8238@gmail.com>
2018-06-06 11:22:26 -0700

    added test/notes-test.js and began setting up tests

--- routes/test.js
+++ routes/test.js
@@ -23,12 +23,35 @@ const Note = require('../models/note');
 // });
 
 
+// router.get('/', (req, res, next) => {
+// 	Note.find()
+// 		.sort({createdAt: -1})
+// 		.then(sorted => {
+// 			return sorted.map((note, index) => {
+// 				note.createdAt = index + 1;
+// 				Note.update(note);
+// 				return note;
+// 			});
+// 		})
+// 		.then(results => {
+// 			console.log(results);
+// 			if(results) {
+// 				res.json(results);
+// 			} else {
+// 				next();
+// 			}
+// 		})
+// 		.catch(err => next(err));
+// });
+
+
 router.get('/', (req, res, next) => {
 	Note.find()
 		.sort({createdAt: -1})
 		.then(sorted => {
-			return sorted.map((note, index) => {
-				note.createdAt = index + 1;
+			return sorted.map((note) => {
+				let newDate = new Date();
+				note.createdAt = newDate.setDate(newDate.getDate() + 1);
 				Note.update(note);
 				return note;
 			});
